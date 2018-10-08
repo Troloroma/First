f003e38d5df8dd000cf37787ad7c65621e20dbcb
Troloroma <rsamarar13@gmail.com>
2018-10-08 22:57:38 +0300

    TestGit

--- projectOne/src/Program.java
+++ projectOne/src/Program.java
@@ -3,9 +3,11 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class Program {
     private static char [][] field = new char[8][8];
+    private static boolean kingDead = false;
+    private static boolean whiteTurn = true;
+
     public static void main(String[] args) throws IOException {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -13,27 +15,114 @@ public class Program {
             }
         }
         Scanner in = new Scanner(System.in);
-        int prX = 0, prY = 1;
         printFigures();
         printField(field);
 
-        while (true) {
-            System.out.println("Ходи!");
-            String s = in.next();
-            int pY = s.charAt(0) - 'A', pX = s.charAt(1) - '1';
-            System.out.println(pX + " " + pY);
-            if (true) {
-                field[pX][pY] = '♞';
-                field[prX][prY] = '-';
-                prX = pX;
-                prY = pY;
-                printField(field);
-            } else {
-                System.out.println("Такой ход невозможен!");
-                continue;
+        while (!kingDead) {
+            if(whiteTurn){
+                System.out.println("\nХод белых!");
+                System.out.println("Кем ходить?");
+                String whom = in.next();
+                int prX = whom.charAt(1) - '1', prY = whom.charAt(0) - 'A';
+                System.out.println("\nКуда ходить?");
+                String where = in.next();
+                int pX = where.charAt(1) - '1', pY = where.charAt(0) - 'A';
+                if (field[prX][prY] == 'L') {
+                    field[pX][pY] = 'L';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = false;
+                }
+                if (field[prX][prY] == 'H') {
+                    field[pX][pY] = 'H';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = false;
+                }
+                if (field[prX][prY] == 'F') {
+                    field[pX][pY] = 'F';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = false;
+                }
+                if (field[prX][prY] == 'D') {
+                    field[pX][pY] = 'D';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = false;
+                }
+                if (field[prX][prY] == 'K') {
+                    field[pX][pY] = 'K';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = false;
+                }
+                if (field[prX][prY] == 'P') {
+                    field[pX][pY] = 'P';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = false;
+                }
+                else {
+                    System.out.println("Такой ход невозможен!");
+                    printField(field);
+                    continue;
+                }
+                System.out.println("x = "+ pY + "\n" + "y = "+ pX);
+            }
+            else if(!whiteTurn){
+                System.out.println("\nХод черных!");
+                System.out.println("Кем ходить?");
+                String whom = in.next();
+                int prX = whom.charAt(1) - '1', prY = whom.charAt(0) - 'A';
+                System.out.println("\nКуда ходить?");
+                String where = in.next();
+                int pX = where.charAt(1) - '1', pY = where.charAt(0) - 'A';
+                if (field[prX][prY] == 'l') {
+                    field[pX][pY] = 'l';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = true;
+                }
+                if (field[prX][prY] == 'h') {
+                    field[pX][pY] = 'h';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = true;
+                }
+                if (field[prX][prY] == 'f') {
+                    field[pX][pY] = 'f';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = true;
+                }
+                if (field[prX][prY] == 'd') {
+                    field[pX][pY] = 'd';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = true;
+                }
+                if (field[prX][prY] == 'k') {
+                    field[pX][pY] = 'k';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = true;
+                }
+                if (field[prX][prY] == 'p') {
+                    field[pX][pY] = 'p';
+                    field[prX][prY] = '_';
+                    printField(field);
+                    whiteTurn = true;
+                }
+                else {
+                    System.out.println("Такой ход невозможен!");
+                    printField(field);
+                    continue;
+                }
+                System.out.println("x = "+ pY + "\n" + "y = "+ pX);
+            }
             }
         }
-    }
 
         private static void printField ( char[][] a){
             System.out.print(" |  ");
